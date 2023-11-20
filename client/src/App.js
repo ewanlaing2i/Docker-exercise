e3746cb0ac5ef804593ec44782b7d53919d2e4ab
Ewan Laing <ewan.laing@2itesting.com>
2023-11-20 12:16:41 +0000

    removed old code

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,10 @@ import ItemList from "./components/ItemList";
 import CommentList from "./components/CommentList";
 
 function App() {
-  // functions and logic
   const [items, setItems] = useState([]);
   const [comments, setComments] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://fakestoreapi.com/products")
-  //     .then((response) => response.json())
-  //     .then((data) => setItems(data));
-  // }, []);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:9000/api/comments")
-  //     .then((response) => response.json())
-  //     .then((data) => setComments(data));
-  // }, []);
 
   useEffect(() => {
     fetch("http://localhost:9000/getItems")
